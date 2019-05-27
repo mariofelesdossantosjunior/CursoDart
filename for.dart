694a0d43fe82e87d1817438d1332e70fefb3a6d6
mario <mario_feles@live.com>
2019-05-26 22:36:22 -0300

    for

---
+++ for.dart
@@ -0,0 +1,10 @@
+main(List<String> args) {
+  for (var i = 0; i < 10; i++) {
+    print(i + 2 * 3);
+  }
+
+  List listaProdutos = ["Arroz", 'Feijão', 'Macarrão'];
+  for (var item in listaProdutos) {
+    print(item);
+  }
+}
